82989d02b3ae7d89ee8f55e387755790df2d4015
Daniel Carvalho <daniel.devop@gmail.com>
2023-09-03 19:03:08 -0300

    timer e botões na mesma linha

--- src/components/FormTask.vue
+++ src/components/FormTask.vue
@@ -5,17 +5,19 @@
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
       </div>
       <div class="column">
-        <section>
-          <strong>00:00:00</strong>
-        </section>
-        <button class="button">
-          <span class="icon"><i class="fas fa-play"></i></span>
-          <span>play</span>
-        </button>
-        <button class="button">
-          <span class="icon"><i class="fas fa-stop"></i></span>
-          <span>stop</span>
-        </button>
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="icon"><i class="fas fa-play"></i></span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon"><i class="fas fa-stop"></i></span>
+            <span>stop</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
